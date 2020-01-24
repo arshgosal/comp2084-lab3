d6bf15143dfe3c429ecaf89135ed5abfe386298a
arsh.gosal <deeparsh.da@gmail.com>
2020-01-23 20:57:55 -0500

    Add project files.

---
+++ PhoneApplicaton/Controllers/PhoneController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PhoneApplicaton.Models;
+
+namespace PhoneApplication.Controllers
+{
+    public class PhoneController : Controller
+    {
+        // GET: Phone
+        public ActionResult Index()
+        {
+            var phoneList = new List<Phone>
+            {
+                new Phone { PhoneID = 1212, PhoneName = "Iphone", Manufacturer = "Apple", MSRP = 1200,ScreenSize = 6.5, DateReleased = DateTime.Parse("09-20-2019")}
+                
+            };
+            return View(phoneList);
+        }
+    }
+}
---
+++ PhoneApplicaton/Models/Phone.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhoneApplicaton.Models
+{
+    public class Phone
+    {
+        public Phone()
+        {
+            PhoneID = 0;
+            PhoneName = "";
+            Manufacturer = "";
+            MSRP = 0;
+            ScreenSize = 0;
+            DateReleased = DateTime.Now;
+        }
+
+        public int PhoneID { get; set; }
+        public string PhoneName { get; set; }
+        public string Manufacturer { get; set; }
+        public double MSRP { get; set; }
+        public double ScreenSize { get; set; }
+        public DateTime DateReleased { get; set; }
+    }
+}
