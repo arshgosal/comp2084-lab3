982e2c49b39ecb2ae4c86991c67a6d8583577195
arsh.gosal <deeparsh.da@gmail.com>
2020-02-06 17:56:11 -0500

    completed lab4

--- PhoneApplicaton/Models/Phone.cs
+++ PhoneApplicaton/Models/Phone.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 
 namespace PhoneApplicaton.Models
 {
@@ -11,16 +14,18 @@ namespace PhoneApplicaton.Models
         {
             PhoneID = 0;
             PhoneName = "";
-            Manufacturer = "";
             MSRP = 0;
             ScreenSize = 0;
             DateReleased = DateTime.Now;
         }
-
+        [Key]
         public int PhoneID { get; set; }
         public string PhoneName { get; set; }
-        public string Manufacturer { get; set; }
+        
+        public virtual Manufacturer Manufacturer { get; set; }
         public double MSRP { get; set; }
+        [ForeignKey("Manufacturer")]
+        public int ManufacturerID { get; set; }
         public double ScreenSize { get; set; }
         public DateTime DateReleased { get; set; }
     }
